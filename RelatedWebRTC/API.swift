50f233d77ef45054ef286b0f9829c76033074868
Carlos Antunes <carlsochfa@gmail.com>
2020-04-16 09:19:50 -0300

    Adding Apollo GraphQL to project.

---
+++ RelatedWebRTC/API.swift
@@ -0,0 +1,9 @@
+//
+//  API.swift
+//  RelatedWebRTC
+//
+//  Created by Carlos Henrique Antunes on 4/16/20.
+//  Copyright © 2020 Carlos Henrique Antunes. All rights reserved.
+//
+
+import Foundation
